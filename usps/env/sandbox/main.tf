e80dee24f506a89fabbfbd69b69ef8a6719d3aee
simon-stathealth <simon@stat-health.com>
2024-01-18 14:54:40 +0300

    add base code for usps-sandbox

---
+++ usps/env/sandbox/main.tf
@@ -0,0 +1,32 @@
+terraform {
+  backend "remote"{
+    hostname = "app.terraform.io"
+    organization = "usps-sanbox"
+    workspaces {
+      name = "usps-sandbox"
+    }
+  }
+    required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "5.12.0"
+    }
+    google-beta = {
+        source = "hashicorp/google-beta" #some modules require google-beta
+        version = "5.12.0"
+
+    }    
+  }
+
+}
+#Google provider
+provider "google" {
+    credentials = var.GCP_JSON_KEY
+    project = var.gcp_project_id
+    region = var.primary_region
+}
+provider "google-beta" {
+    credentials = var.GCP_JSON_KEY
+    project = var.gcp_project_id
+    region = var.primary_region  
+}
---
+++ usps/env/sandbox/variables.tf
@@ -0,0 +1,12 @@
+variable "gcp_project_id" {
+    description = "GCP Project ID"
+    type = string
+}
+variable "primary_region" {
+    description = "Primary region for GCP resources"
+    type = string
+}
+variable "GCP_JSON_KEY" {
+  description = "GCP JSON Key"
+    type = string
+}
