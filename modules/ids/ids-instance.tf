b2c5ab14942eb23e23692d0f001ab987245a330a
simon-stathealth <simon@stat-health.com>
2024-01-19 01:21:14 +0300

    add update to ids

--- modules/ids/ids-instance.tf
+++ modules/ids/ids-instance.tf
@@ -3,5 +3,5 @@ resource "google_cloud_ids_endpoint" "example-endpoint" {
     location = "${var.primary_region}-f"
     network  = data.google_compute_network.vpc_network.id
     severity = "INFORMATIONAL"
-    depends_on = [data.google_service_networking_connection.private_service_connection]
+    depends_on = ["servicenetworking.googleapis.com"]
 }
