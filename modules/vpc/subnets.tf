1a17d10f3b2839d72800a086c71fd1a49b9c7cdc
simon-stathealth <simon@stat-health.com>
2024-01-18 18:56:56 +0300

    update region configs

--- modules/vpc/subnets.tf
+++ modules/vpc/subnets.tf
@@ -1,20 +1,20 @@
 resource "google_compute_subnetwork" "subnet_zone1" {
     name          = "subnet-zone1"
     ip_cidr_range = "${var.ip_cidr_range_sub1}"
-    region        = "us-central1"
+    region        = "${var.primary_region}"
     network       = google_compute_network.vpc_network.self_link
 }
 
 resource "google_compute_subnetwork" "subnet_zone2" {
     name          = "subnet-zone2"
     ip_cidr_range = "${var.ip_cidr_range_sub2}"
-    region        = "us-central1"
+    region        = "${var.primary_region}"
     network       = google_compute_network.vpc_network.self_link
 }
 
 resource "google_compute_subnetwork" "subnet_zone3" {
     name          = "subnet-zone3"
     ip_cidr_range = "${var.ip_cidr_range_sub3}"
-    region        = "us-central1"
+    region        = "${var.primary_region}"
     network       = google_compute_network.vpc_network.self_link
 }
