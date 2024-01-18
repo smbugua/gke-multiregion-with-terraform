c62ec101e4f58c81cc88d10c0262e1f030db76fa
simon-stathealth <simon@stat-health.com>
2024-01-18 17:45:51 +0300

    use version 5.5.0

--- env/sandbox/us-central1/main.tf
+++ env/sandbox/us-central1/main.tf
@@ -9,11 +9,11 @@ terraform {
     required_providers {
     google = {
       source = "hashicorp/google"
-      version = "5.12.0"
+      version = "5.5.0"
     }
     google-beta = {
         source = "hashicorp/google-beta" #some modules require google-beta
-        version = "5.12.0"
+        version = "5.5.0"
 
     }    
   }
