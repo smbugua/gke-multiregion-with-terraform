399284ae401be82ef967acae3dea3f6b68556aa7
simon-stathealth <simon@stat-health.com>
2024-01-19 01:21:23 +0300

    rename ids resource

--- modules/ids/ids-instance.tf
+++ modules/ids/ids-instance.tf
@@ -1,4 +1,4 @@
-resource "google_cloud_ids_endpoint" "example-endpoint" {
+resource "google_cloud_ids_endpoint" "ids-endpoint" {
     name     = "ids-endpoint-${var.primary_region}"
     location = "${var.primary_region}-f"
     network  = data.google_compute_network.vpc_network.id
