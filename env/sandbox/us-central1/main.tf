d8e8deca95dca38c6176531828e8db1de5e53c65
simon-stathealth <simon@stat-health.com>
2024-01-18 17:37:45 +0300

    activate vpc module

--- env/sandbox/us-central1/main.tf
+++ env/sandbox/us-central1/main.tf
@@ -33,3 +33,14 @@ provider "google-beta" {
     project = var.gcp_project_id
     region = var.primary_region  
 }
+
+#activate the modules
+module "vpc" {
+    source = "../../modules/vpc"
+    gcp_project_id = var.gcp_project_id
+    primary_region = var.primary_region
+    ip_cidr_range = var.ip_cidr_range
+    ip_cidr_range_sub1 = var.ip_cidr_range_sub1
+    ip_cidr_range_sub2 = var.ip_cidr_range_sub2
+    ip_cidr_range_sub3 = var.ip_cidr_range_sub3
+}
