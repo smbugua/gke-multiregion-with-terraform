912d24ff1f67a9551b2f50e133236c8ce6216196
simon-stathealth <simon@stat-health.com>
2024-01-23 14:31:51 +0300

    remove defaut node pool config

--- modules/gke/kubernetes-cluster.tf
+++ modules/gke/kubernetes-cluster.tf
@@ -1,7 +1,6 @@
 resource "google_container_cluster" "my_cluster" {
     name               = "usps-autopilot-cluster"
     location           = "${var.primary_region}"
-    remove_default_node_pool = true
     network = "usps-sandbox-${var.primary_region}-network"
     subnetwork = "usps-sandbox-${var.primary_region}-subnet"
 
