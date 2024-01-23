9e797dd5cb646917c195f2a99bb6235d9146fed1
simon-stathealth <simon@stat-health.com>
2024-01-23 14:43:32 +0300

    remove subnet zones

--- modules/gke/kubernetes-cluster.tf
+++ modules/gke/kubernetes-cluster.tf
@@ -2,7 +2,6 @@ resource "google_container_cluster" "my_cluster" {
     name               = "usps-autopilot-cluster"
     location           = "${var.primary_region}"
     network = "usps-sandbox-${var.primary_region}-network"
-    subnetwork = "usps-sandbox-${var.primary_region}-subnet"
 
     ip_allocation_policy {
         cluster_secondary_range_name = "pods"
