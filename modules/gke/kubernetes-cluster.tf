f91d7529db17cc74cd56129bd7fbf76273fd629b
simon-stathealth <simon@stat-health.com>
2024-01-23 14:30:23 +0300

    auto pilot settings

--- modules/gke/kubernetes-cluster.tf
+++ modules/gke/kubernetes-cluster.tf
@@ -3,34 +3,7 @@ resource "google_container_cluster" "my_cluster" {
     location           = "${var.primary_region}"
     remove_default_node_pool = true
     network = "usps-sandbox-${var.primary_region}-network"
-    node_pool {
-        name               = "main-node-pool"
-        initial_node_count = 1
-        autoscaling {
-            min_node_count = 1
-            max_node_count = 10
-        }
-        management {
-            auto_repair  = true
-            auto_upgrade = true
-        }
-        node_config {
-            machine_type = "e2-medium"
-            disk_size_gb = 100
-            oauth_scopes = [
-                "https://www.googleapis.com/auth/compute",
-                "https://www.googleapis.com/auth/devstorage.read_only",
-                "https://www.googleapis.com/auth/logging.write",
-                "https://www.googleapis.com/auth/monitoring",
-            ]
-        }
-    }
-
-    addons_config {
-        network_policy_config {
-            disabled = true
-        }
-    }
+    subnetwork = "usps-sandbox-${var.primary_region}-subnet"
 
     ip_allocation_policy {
         cluster_secondary_range_name = "pods"
