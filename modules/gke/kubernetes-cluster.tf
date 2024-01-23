150a3b5bb375a53b5378f9c59f591bb56d6a7863
simon-stathealth <simon@stat-health.com>
2024-01-23 14:27:50 +0300

    add network config

--- modules/gke/kubernetes-cluster.tf
+++ modules/gke/kubernetes-cluster.tf
@@ -2,7 +2,7 @@ resource "google_container_cluster" "my_cluster" {
     name               = "usps-autopilot-cluster"
     location           = "${var.primary_region}"
     remove_default_node_pool = true
-
+    network = "usps-sandbox-${var.primary_region}-network"
     node_pool {
         name               = "main-node-pool"
         initial_node_count = 1
