2c6434a34ac5a49dbe3bb30c47e2125d17511aad
simon-stathealth <simon@stat-health.com>
2024-01-23 14:50:49 +0300

    uodate subnet

--- modules/gke/kubernetes-cluster.tf
+++ modules/gke/kubernetes-cluster.tf
@@ -2,6 +2,7 @@ resource "google_container_cluster" "my_cluster" {
     name               = "usps-autopilot-cluster"
     location           = "${var.primary_region}"
     network = "usps-sandbox-${var.primary_region}-network"
+    subnetwork = "subnet-zone1"
 
     ip_allocation_policy {
         cluster_secondary_range_name = "pods"
