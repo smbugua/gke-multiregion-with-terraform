f4dfb4c1e3557327c9cef44ab77a9e9846097362
simon-stathealth <simon@stat-health.com>
2024-01-24 20:19:22 +0300

    kill ids on zone 2 also

--- env/sandbox/us-east1/main.tf
+++ env/sandbox/us-east1/main.tf
@@ -40,11 +40,11 @@ module "gcp-vpc" {
     ip_cidr_range_sub2 = var.ip_cidr_range_sub2
     ip_cidr_range_sub3 = var.ip_cidr_range_sub3
 }
-module "gcp-ids" {
-    source = "../../../modules/ids"
-    gcp_project_id = var.gcp_project_id
-    primary_region = var.primary_region
-}
+# module "gcp-ids" {
+#     source = "../../../modules/ids"
+#     gcp_project_id = var.gcp_project_id
+#     primary_region = var.primary_region
+# }
 module "gcp-gke" {
     source = "../../../modules/gke"
     gcp_project_id = var.gcp_project_id
