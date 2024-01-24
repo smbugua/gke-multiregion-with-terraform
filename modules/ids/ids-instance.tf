d0b4072befc1786d07e72d20b4a52a349024b5a8
simon-stathealth <simon@stat-health.com>
2024-01-24 20:14:35 +0300

    change region in ids zone

--- modules/ids/ids-instance.tf
+++ modules/ids/ids-instance.tf
@@ -1,6 +1,6 @@
 resource "google_cloud_ids_endpoint" "ids-endpoint" {
     name     = "ids-endpoint-${var.primary_region}"
-    location = "${var.primary_region}-f"
+    location = "${var.primary_region}-b"
     network  = data.google_compute_network.vpc_network.id
     severity = "INFORMATIONAL"
 }
